74fb1f7a1c88156750a95bed27fb1b2823dd2555
DevFelp <paulofelipe06p@gmail.com>
2024-06-09 14:38:47 -0300

    Versão 2.0

--- src/contador.java
+++ src/contador.java
@@ -2,18 +2,29 @@ import java.util.Scanner;
 public class contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro:");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro:");
-        int parametroDois = terminal.nextInt();
+        boolean loopSystem = true;
+        boolean loopException = true;
+        do {
+            do {
+                System.out.println("Digite o primeiro parâmetro:");
+                int parametroUm = terminal.nextInt();
+                System.out.println("Digite o segundo parâmetro:");
+                int parametroDois = terminal.nextInt();
 
-        try {
-            contar(parametroUm, parametroDois);
-        } catch (ParametrosInvalidoException e) {
-            System.out.println(e.getMessage());
-        }finally {
-            terminal.close();
-        }
+                try {
+                    contar(parametroUm, parametroDois);
+                    loopException = false;
+                } catch (ParametrosInvalidoException e) {
+                    System.out.println(e.getMessage());
+                    loopException = true;
+                }
+                System.out.println("Deseja fazer uma nova conatagem? (S/N)");
+                String respostaUser = terminal.next();
+                loopSystem = respostaUser.equalsIgnoreCase("S");
+            } while (loopException);
+
+        }while(loopSystem);
+terminal.close();
     }
 
 static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidoException {
